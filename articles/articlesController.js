b81420768509cc484f298a5150b6aceef3b669d2
ivan <ivan-guita@hotmail.com>
2023-05-17 08:03:08 -0300

    funções de deletar concluidas e tiny mce instalado

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -1,17 +1,94 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../categories/Category");
+const Article = require('./Article');
+const slugify = require('slugify');
 
-router.get("/articles",(req,res)=>{
+router.get("/admin/articles",(req,res)=>{
+  //desnto do find all eu digo quais as tebelas que eu quero incluir 
+  //no caso eu mandei incluir categorias pra pegar so o titulo delas 
 
-  res.send("Rota de articles");
+  Article.findAll({
+      include:[{model: Category}]
+  }).then(articles =>{
+
+    res.render("admin/articles/index",{articles: articles});
+
+  })
 
 });
 
+
+
 router.get("/admin/articles/new",(req,res)=>{
+    
+    Category.findAll().then(categories =>{
 
-    res.send("Rota para criar um novo artigo");
+      res.render("admin/articles/new",{categories: categories});
+      
+    })    
+});
 
-  });
+
+
+router.post("/articles/save",(req,res)=>{
+  
+   var title = req.body.title;
+   var body  = req.body.body;
+   var category = req.body.category;
+
+
+   Article.create({
+     title: title,
+     slug: slugify(title),
+     body: body,
+     categoryId: category
+
+   }).then(()=>{
+
+     res.redirect("/admin/articles");
+
+   })
+});
+
+
+router.post("/articles/delete", (req,res) => {
+
+  var id = req.body.id;
+
+  if(id != undefined){ //testando se o id e diferente de undefined
+
+    if(!isNaN(id)){ //testando se o id que vai vir e um numero 
+
+      //se ele for diferente de indefinido e for um numero 
+
+      Article.destroy({
+       where:{
+          id:id
+       }
+     }).then(()=>{
+
+       res.redirect('/admin/articles');
+          
+     })
+
+       
+
+    }else{
+
+       res.redirect('/admin/articles');
+    }
+
+
+  }else{
+     res.redirect('/admin/categories')
+  }
+
+
+});
+
+
+
 
 module.exports = router;
   
