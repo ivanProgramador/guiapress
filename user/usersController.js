0b43ac0ce6878a59c413884bbde701749ac39474
ivan <ivan-guita@hotmail.com>
2023-05-26 09:49:57 -0300

    rota que transforma senha em uma rash e salva na base de dados concluida

--- user/usersController.js
+++ user/usersController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./User');
+const bcrypt = require('bcryptjs');
 
 router.get('/admin/users',(req,res)=>{
 
@@ -16,7 +17,30 @@ router.post('/users/create',(req,res)=>{
     var email = req.body.email;
     var password = req.body.password;
 
-    res.json({email,password})
+    //nunca se deve salvar na base de dados uma senha exatamente como ela foi digitada pelo usuario
+    // exite duas formas de codifica-la a forma menos segura e a criptografia porque a criptografia
+    //segue padrões e existem regras de decodificação que podem ser aplicadas para pegar as senhas dos usuarios 
+    //a forma mais segura para este porjeto foi o rash porque não tem extamente um jeito de reverter um rash
+    //para fazer iss eu usei a biblioteca bcryptjs
+
+    var salt = bcrypt.genSaltSync(10); //criando o salt 
+
+    var hash = bcrypt.hashSync(password,salt); //pegando a senha e o salt pra gerar uma hash pra senha  
+
+    //salavndo a rash lgerada com abse na senha no banco de dados e salvando tambem o email 
+    
+     User.create({
+        email: email,
+        password: hash
+
+     }).then(()=>{
+
+        res.redirect("/");
+
+     }).catch((err)=>{
+
+        res.redirect("/");
+     })
 })
 
 module.exports = router;
